6888dd4abd4c0da2ae9a0ef8a2ed3e0eea37cbf0
unknown <you@example.com>
2024-05-03 22:18:12 +0530

    Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,29 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
-const page = () => {
+const Home = () => {
+  const loggedIn = {firstName:'Mjstar'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+            <HeaderBox 
+              type="greeting"
+              title="welcome"
+              user={loggedIn?.firstName || 'Guest'}
+              subtext="Access and manage your account and transaction efficiently."
+            />
+
+            <TotalBalanceBox 
+              accounts = {[]}
+              totalBanks = {1}
+              totalCurrentBalance={1250.35}
+            />
+        </header>
+      </div>
+    </section>
   )
 }
 
-export default page
+export default Home
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react'
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ({amount}:{amount:number}) => {
+  return (
+    <div className="w-full">
+        <CountUp
+            decimals={2} 
+            decimal=","
+            prefix="$"
+            end={amount}
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
