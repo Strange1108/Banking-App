cbc35c576462d4e9c93d28507d5fc7d1a7462bce
unknown <you@example.com>
2024-05-04 18:24:30 +0530

    Auth Page UI

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,19 @@
+'use server';
+
+export const signIn = async () =>
+{
+    try {
+
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+
+export const signUp = async (userData: SignUpParams) =>
+    {
+        try {
+    
+        } catch (error) {
+            console.log('Error', error);
+        }
+    }
