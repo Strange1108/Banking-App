e78ede8ef82c0bedb0712494bf70e387e4c6b59b
unknown <you@example.com>
2024-05-05 00:14:50 +0530

    Auth one

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -29,6 +29,7 @@ const AuthForm = ({type} : {type : string}) => {
     const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    
 
     const formSchema = authFormSchema(type);
 
@@ -47,18 +48,17 @@ const AuthForm = ({type} : {type : string}) => {
                 
                 if(type === 'sign-up')
                 {
-                    // const newUser = await signUp(data);
-
-                    // setUser(newUser);
+                    const newUser = await signUp(data);
+                    setUser(newUser);
                 }
 
                 if(type === 'sign-in')
                 {
-                    // const response = await signIn({
-                    //     email: data.email,
-                    //     password: data.password,
-                    // })
-                    // if(response) router.push('/')
+                    const response = await signIn({
+                        email: data.email,
+                        password: data.password,
+                    })
+                    if(response) router.push('/')
                 }
             }catch(error) {
                 console.log(error);
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,19 +1,59 @@
 'use server';
 
-export const signIn = async () =>
+import { cookies } from "next/headers";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
+import { resolve } from "path";
+
+export const signIn = async ({email, password}: signInProps) =>
 {
     try {
-
+        const { account } = await createAdminClient();
+        const response = await account.createEmailPasswordSession(email, password);
+        return parseStringify(response);
     } catch (error) {
         console.log('Error', error);
     }
 }
 
 export const signUp = async (userData: SignUpParams) =>
-    {
-        try {
-    
-        } catch (error) {
-            console.log('Error', error);
-        }
+{
+    const {email, password, firstName, lastName} = userData;
+    try {
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            `${firstName} ${lastName}`
+        );
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify(newUserAccount);
+
+    } catch (error) {
+        console.log('Error', error);
     }
+}
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      const user = await account.get();
+      return parseStringify(user);
+    } catch (error) {
+      return null;
+    }
+  }
+  
